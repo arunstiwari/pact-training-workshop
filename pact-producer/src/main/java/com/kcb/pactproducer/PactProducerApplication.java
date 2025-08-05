59820f01315e6e37ed0653fc3c826e67c3eb4ab2
arun.tiwari123 <arunstiwari@gmail.com>
2025-08-05 11:51:59 +0530

    Initial Setup for Pact Workshop

---
+++ pact-producer/src/main/java/com/kcb/pactproducer/PactProducerApplication.java
@@ -0,0 +1,13 @@
+package com.kcb.pactproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PactProducerApplication.class, args);
+    }
+
+}
---
+++ pact-producer/src/test/java/com/kcb/pactproducer/PactProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcb.pactproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
